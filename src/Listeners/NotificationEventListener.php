67dc3464e901b91d35cdf64bed3ac2fb0e81d88c
roxanariza <roxana_r2000@yahoo.com>
2020-04-16 17:21:32 +0300

    Check if user should receive notifications, based on settings

--- src/Listeners/NotificationEventListener.php
+++ src/Listeners/NotificationEventListener.php
@@ -7,7 +7,9 @@ use Doctrine\ORM\ORMException;
 use Illuminate\Support\Facades\Event;
 use Railroad\Railnotifications\Contracts\ContentProviderInterface;
 use Railroad\Railnotifications\Contracts\RailforumProviderInterface;
+use Railroad\Railnotifications\Contracts\UserProviderInterface;
 use Railroad\Railnotifications\Entities\Notification;
+use Railroad\Railnotifications\Exceptions\BroadcastNotificationFailure;
 use Railroad\Railnotifications\Services\NotificationBroadcastService;
 use Railroad\Railnotifications\Services\NotificationService;
 
@@ -34,6 +36,11 @@ class NotificationEventListener
     private $railforumProvider;
 
     /**
+     * @var UserProviderInterface
+     */
+    private $userProvider;
+
+    /**
      * NotificationEventListener constructor.
      *
      * @param NotificationService $notificationService
@@ -43,19 +50,22 @@ class NotificationEventListener
     public function __construct(
         NotificationService $notificationService,
         NotificationBroadcastService $notificationBroadcastService,
-        ContentProviderInterface $contentProvider, RailforumProviderInterface $railforumProvider
+        ContentProviderInterface $contentProvider,
+        RailforumProviderInterface $railforumProvider,
+        UserProviderInterface $userProvider
     ) {
         $this->notificationService = $notificationService;
         $this->notificationBroadcastService = $notificationBroadcastService;
         $this->contentProvider = $contentProvider;
         $this->railforumProvider = $railforumProvider;
+        $this->userProvider = $userProvider;
     }
 
     /**
      * @param Event $event
      * @throws ORMException
      * @throws OptimisticLockException
-     * @throws \Railroad\Railnotifications\Exceptions\BroadcastNotificationFailure
+     * @throws BroadcastNotificationFailure
      */
     public function handle(Event $event)
     {
@@ -72,27 +82,66 @@ class NotificationEventListener
             case Notification::TYPE_LESSON_COMMENT_REPLY:
                 $comment = $this->contentProvider->getCommentById($event->data['commentId']);
                 $originalComment = $this->contentProvider->getCommentById($comment['parent_id']);
-                $receivingUserIds = ($originalComment)?[$originalComment['user_id']]:[];
+                $receivingUserIds = ($originalComment) ? [$originalComment['user_id']] : [];
                 break;
             case Notification::TYPE_LESSON_COMMENT_LIKED:
-                    $comment = $this->contentProvider->getCommentById($event->data['commentId']);
-                    $receivingUserIds = [$comment['user_id']];
-                    break;
+                $comment = $this->contentProvider->getCommentById($event->data['commentId']);
+                $receivingUserIds = [$comment['user_id']];
+                break;
             default:
                 $receivingUserIds = [];
         }
 
-        foreach($receivingUserIds as $receivingUserId) {
+        foreach ($receivingUserIds as $receivingUserId) {
             // create the notification
             $notification = $this->notificationService->create(
                 $event->type,
                 $event->data,
                 $receivingUserId
             );
+            $user = $this->userProvider->getRailnotificationsUserById($receivingUserId);
 
-            foreach (array_keys(config('railnotifications.channels')) as $channel) {
-                $this->notificationBroadcastService->broadcast($notification->getId(), $channel);
+            $broadcastChannels = array_keys(config('railnotifications.channels'));
+            //check if are instant notifications
+            if (!empty($user->getNotificationsSummaryFrequencyMinutes())) {
+                $broadcastChannels = 'fcm';
+            }
+
+            $shouldReceiveNotification = $this->shouldReceiveNotification($user, $event->type);
+            if($shouldReceiveNotification) {
+                foreach ($broadcastChannels as $channel) {
+                    $this->notificationBroadcastService->broadcast($notification->getId(), $channel);
+                }
             }
         }
     }
+
+    /**
+     * @param $user
+     * @param $type
+     * @return bool
+     */
+    private function shouldReceiveNotification($user, $type)
+    {
+        switch ($type) {
+            case Notification::TYPE_FORUM_POST_IN_FOLLOWED_THREAD:
+                $shouldReceive = $user->getNotifyOnForumFollowedThreadReply();
+                break;
+            case Notification::TYPE_FORUM_POST_REPLY:
+                $shouldReceive = $user->getNotifyOnForumPostReply();
+                break;
+            case Notification::TYPE_LESSON_COMMENT_REPLY:
+                $shouldReceive = $user->getNotifyOnLessonCommentReply();
+                break;
+            case Notification::TYPE_LESSON_COMMENT_LIKED:
+                $shouldReceive = $user->getNotifyOnLessonCommentLike();
+                break;
+            default:
+                $shouldReceive = true;
+        }
+
+        return $shouldReceive;
+    }
+
 }
+

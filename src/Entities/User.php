82203c91c8623581bd7815047344c73010d2c247
roxanariza <roxana_r2000@yahoo.com>
2020-04-16 17:21:01 +0300

    Add user notification settings

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -33,6 +33,20 @@ class User implements UserEntityInterface
      */
     private $avatar;
 
+    private $notifyOnLessonCommentReply;
+
+    private $notifyWeeklyUpdate;
+
+    private $notifyOnForumPostLike;
+
+    private $notifyOnForumFollowedThreadReply;
+
+    private $notifyOnForumPostReply;
+
+    private $notifyOnLessonCommentLike;
+
+    private $notificationsSummaryFrequencyMinutes;
+
     /**
      * User constructor.
      *
@@ -40,17 +54,38 @@ class User implements UserEntityInterface
      * @param $email
      * @param $displayName
      * @param $avatar
+     * @param $notifyOnLessonCommentReply
+     * @param $notifyWeeklyUpdate
+     * @param $notifyOnForumPostLike
+     * @param $notifyOnForumFollowedThreadReply
+     * @param $notifyOnForumPostReply
+     * @param $notifyOnLessonCommentLike
+     * @param $notificationsSummaryFrequencyMinutes
      */
     public function __construct(
         int $id,
         $email,
         $displayName,
-        $avatar
+        $avatar,
+        $notifyOnLessonCommentReply,
+        $notifyWeeklyUpdate,
+        $notifyOnForumPostLike,
+        $notifyOnForumFollowedThreadReply,
+        $notifyOnForumPostReply,
+        $notifyOnLessonCommentLike,
+        $notificationsSummaryFrequencyMinutes
     ) {
         $this->id = $id;
         $this->email = $email;
         $this->displayName = $displayName;
         $this->avatar = $avatar;
+        $this->notifyOnLessonCommentReply = $notifyOnLessonCommentReply;
+        $this->notifyWeeklyUpdate = $notifyWeeklyUpdate;
+        $this->notifyOnForumPostLike = $notifyOnForumPostLike;
+        $this->notifyOnForumFollowedThreadReply = $notifyOnForumFollowedThreadReply;
+        $this->notifyOnForumPostReply = $notifyOnForumPostReply;
+        $this->notifyOnLessonCommentLike = $notifyOnLessonCommentLike;
+        $this->notificationsSummaryFrequencyMinutes = $notificationsSummaryFrequencyMinutes;
     }
 
     /**
@@ -113,13 +148,124 @@ class User implements UserEntityInterface
         return $this->avatar;
     }
 
-
     /**
      * @param $avatar
      */
     public function setAvatar($avatar)
     : void {
         $this->avatar = $avatar;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getNotifyOnLessonCommentReply()
+    {
+        return $this->notifyOnLessonCommentReply;
+    }
+
+    /**
+     * @param bool $notifyOnLessonCommentReply
+     */
+    public function setNotifyOnLessonCommentReply($notifyOnLessonCommentReply)
+    {
+        $this->notifyOnLessonCommentReply = $notifyOnLessonCommentReply;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getNotifyWeeklyUpdate()
+    {
+        return $this->notifyWeeklyUpdate;
+    }
+
+    /**
+     * @param bool $notifyWeeklyUpdate
+     */
+    public function setNotifyWeeklyUpdate($notifyWeeklyUpdate)
+    {
+        $this->notifyWeeklyUpdate = $notifyWeeklyUpdate;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getNotifyOnForumPostLike()
+    {
+        return $this->notifyOnForumPostLike;
+    }
+
+    /**
+     * @param bool $notifyOnForumPostLike
+     */
+    public function setNotifyOnForumPostLike($notifyOnForumPostLike)
+    {
+        $this->notifyOnForumPostLike = $notifyOnForumPostLike;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getNotifyOnForumFollowedThreadReply()
+    {
+        return $this->notifyOnForumFollowedThreadReply;
+    }
+
+    /**
+     * @param bool $notifyOnForumFollowedThreadReply
+     */
+    public function setNotifyOnForumFollowedThreadReply($notifyOnForumFollowedThreadReply)
+    {
+        $this->notifyOnForumFollowedThreadReply = $notifyOnForumFollowedThreadReply;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getNotifyOnForumPostReply()
+    {
+        return $this->notifyOnForumPostReply;
+    }
+
+    /**
+     * @param bool $notifyOnForumPostReply
+     */
+    public function setNotifyOnForumPostReply($notifyOnForumPostReply)
+    {
+        $this->notifyOnForumPostReply = $notifyOnForumPostReply;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getNotifyOnLessonCommentLike()
+    {
+        return $this->notifyOnLessonCommentLike;
+    }
+
+    /**
+     * @param bool $notifyOnLessonCommentLike
+     */
+    public function setNotifyOnLessonCommentLike($notifyOnLessonCommentLike)
+    {
+        $this->notifyOnLessonCommentLike = $notifyOnLessonCommentLike;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getNotificationsSummaryFrequencyMinutes()
+    {
+        return $this->notificationsSummaryFrequencyMinutes;
+    }
+
+    /**
+     * @param int|null $notificationsSummaryFrequencyMinutes
+     */
+    public function setNotificationsSummaryFrequencyMinutes($notificationsSummaryFrequencyMinutes)
+    {
+        $this->notificationsSummaryFrequencyMinutes = $notificationsSummaryFrequencyMinutes;
     }
 
     /**

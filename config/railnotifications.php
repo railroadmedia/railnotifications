83e6f5c2709df0ab69d8c356f02e40c842ea0118
roxanariza <roxana_r2000@yahoo.com>
2020-04-16 17:15:52 +0300

    Update config file

--- config/railnotifications.php
+++ config/railnotifications.php
@@ -1,9 +1,10 @@
 <?php
 
-return [
+return array(
+    //the notifications will be broadcast on all the channels defined bellow
     'channels' => [
-        /* add custom channels here, custom channels must extend a base channel */
-        \Railroad\Railnotifications\Channels\FcmChannel::class
+        'email' => \Railroad\Railnotifications\Channels\EmailChannel::class,
+        'fcm' => \Railroad\Railnotifications\Channels\FcmChannel::class
     ],
 
     // cache
@@ -19,6 +20,7 @@ return [
     'database_host' => 'mysql',
     'database_driver' => 'pdo_mysql',
     'database_in_memory' => false,
+    'enable_query_log' => false,
 
     // entities
     'entities' => [
@@ -27,19 +29,10 @@ return [
             'namespace' => 'Railroad\Railnotifications\Entities',
         ],
     ],
-    'data' => [
-        \Railroad\Railnotifications\Entities\Notification::TYPE_LESSON_COMMENT_LIKED =>[
-            'title' => 'New reply to your comment',
-            'message' => 'New reply to your comment'
-        ],
-        \Railroad\Railnotifications\Entities\Notification::TYPE_LESSON_COMMENT_LIKED => [
-            'title' => 'New like to your comment',
-            'message' => 'New like to your comment'
-        ]
-    ],
     'emailAddressFrom' => 'system@pianote.com',
     'emailBrandFrom' => 'Pianote',
     'replyAddress' => 'suport@pianote.com',
     'newThreadPostSubject' => 'Pianote Forums - New Thread Post: ',
-    'newLessonCommentReplySubject' => 'Pianote - New Lesson Comment Reply: '
-];
+    'newLessonCommentReplySubject' => 'Pianote - New Lesson Comment Reply: ',
+    'newLessonCommentLikedSubject' => 'Pianote - New Lesson Comment Like: '
+);

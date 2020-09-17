bf5b5396a68e9b835b716f0442c978d61d3a1fdb
Roxana Riza <roxana_r2000@yahoo.com>
2020-09-17 16:55:37 +0300

    Rename migration classes

--- migrations/2020_09_17_181248_create_notifications_table.php
+++ migrations/2020_09_17_181248_create_notifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNotificationsTable extends Migration
+class CreateMusoraNotificationsTable extends Migration
 {
     /**
      * Run the migrations.

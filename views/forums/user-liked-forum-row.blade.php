029fc3f1e1116c96e20b1262dd86ea1af843e934
Roxana Riza <roxana_r2000@yahoo.com>
2021-07-12 16:00:55 +0300

    Delete like icon from user-liked forum template

--- views/forums/user-liked-forum-row.blade.php
+++ views/forums/user-liked-forum-row.blade.php
@@ -1,7 +1,4 @@
 <tr style="border-bottom:1px solid #d1d1d1">
-    <td style="max-width:80px;" valign="top">
-        <img style="border-radius:0;" src="{{ cdn('icons/like.png') }}">
-    </td>
     <td style="max-width:99%;padding:30px 15px;">
 
         <h1>{{ $totalLikes == 1? $totalLikes : $totalLikes . ' people'  }} liked your forum post.</h1>
